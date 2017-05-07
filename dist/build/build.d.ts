ee1af5fcd39adbf107e73a446d1d0106ea5e2cb7
GrePuG <grepug@163.com>
2017-05-07 19:21:53 +0800

    ...

--- dist/build/build.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Build {
-    dirs: any;
-    init(): Promise<void>;
-    compileTs(): Promise<void>;
-    compilePug(): Promise<void>;
-    compileLess(): Promise<void>;
-}
--- dist/build/build.js
+++ dist/build/build.js
@@ -1,44 +1,26 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const shell = require('shelljs');
-const util_1 = require("./util");
-const pug = require("pug");
-class Build {
-    init() {
-        return __awaiter(this, void 0, void 0, function* () {
-            this.dirs = yield util_1.readDir();
-        });
-    }
-    compileTs() {
-        return __awaiter(this, void 0, void 0, function* () {
-            shell.exec('tsc');
-        });
-    }
-    compilePug() {
-        return __awaiter(this, void 0, void 0, function* () {
-            for (let d of this.dirs) {
-                let html = pug.renderFile(`${d}/${d}.pug`);
-                yield util_1.writeFile(`dist/pages/${d}/${d}.html`, html);
-            }
-        });
-    }
-    compileLess() {
-        return __awaiter(this, void 0, void 0, function* () {
-            for (let d of this.dirs) {
-                let html = pug.renderFile(`${d}/${d}.pug`);
-                yield util_1.writeFile(`dist/pages/${d}/${d}.html`, html);
-            }
-        });
-    }
-}
-exports.Build = Build;
-new Build().init();
+// const shell = require('shelljs')
+// import { readDir, writeFile } from './util'
+// import pug = require('pug')
+// export class Build {
+//   dirs: any
+//   async init() {
+//     this.dirs = await readDir()
+//   }
+//   async compileTs() {
+//     shell.exec('tsc')
+//   }
+//   async compilePug() {
+//     for (let d of this.dirs) {
+//       let html = pug.renderFile(`${d}/${d}.pug`)
+//       await writeFile(`dist/pages/${d}/${d}.html`, html)
+//     }
+//   }
+//   async compileLess () {
+//     for (let d of this.dirs) {
+//       let html = pug.renderFile(`${d}/${d}.pug`)
+//       await writeFile(`dist/pages/${d}/${d}.html`, html)
+//     }
+//   }
+// }
+// new Build().init() 
 //# sourceMappingURL=build.js.map
